e4f5a98b5f667d6e333914ee28e6d2fb0c79c567
Thomas Mertz <tm@peytz.dk>
2014-09-16 08:56:57 +0200

    Added CPT: confu_signup

--- confu.php
+++ confu.php
@@ -24,7 +24,7 @@ function confu_activate() {
 			'read' => true
 		) 
 	);
-	#remove_role( 'subscriber' );
+	remove_role( 'subscriber' );
 }
 
 function confu_attendees_shortcode(){
@@ -146,6 +146,36 @@ function confu_init() {
 			'hierarchical' => true,
 		)
 	);
+	
+	$labels = array(
+		'name' => _x('Tilmeldinger', 'post type general name'),
+		'singular_name' => _x('Tilmelding', 'post type singular name'),
+		'add_new' => _x('Tilføj ny', 'tilmelding'),
+		'add_new_item' => __('Tilføj ny tilmelding'),
+		'edit_item' => __('Rediger tilmelding'),
+		'new_item' => __('Ny tilmelding'),
+		'all_items' => __('Alle tilmeldinger'),
+		'view_item' => __('Vis tilmelding'),
+		'search_items' => __('Søg i tilmeldinger'),
+		'not_found' =>  __('Ingen tilmeldinger fundet'),
+		'not_found_in_trash' => __('Ingen tilmeldinger fundet i papirkurven'), 
+		'menu_name' => __('Tilmeldinger')
+	);
+	$args = array(
+		'labels' => $labels,
+		'public' => false,
+		'publicly_queryable' => false,
+		'show_ui' => true, 
+		'show_in_menu' => true, 
+		'query_var' => true,
+		'rewrite' => array( 'slug' => 'programmet' ),
+		'capability_type' => 'post',
+		'has_archive' => false, 
+		'hierarchical' => false,
+		'menu_position' => null,
+		'supports' => array( 'title', 'editor', 'thumbnail' )
+	); 
+	register_post_type('confu_signups',$args);
 }
 add_action( 'init', 'confu_init' );
 ?>
